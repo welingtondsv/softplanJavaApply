faaaa174b91b0f0e29b101fd417932d42b80aa3b
welington.vilhalva <welingtondsv78@gmail.com>
2019-11-16 14:16:35 -0400

    basic auth

--- src/main/java/com/javaapply/softplan/SecurityConfig.java
+++ src/main/java/com/javaapply/softplan/SecurityConfig.java
@@ -1,17 +1,43 @@
 package com.javaapply.softplan;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EnableWebSecurity
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()
+				.withUser("softplayer")
+				.password(passwordEncoder().encode("123"))
+				.authorities("ROLE_USER");;
+	}
+
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+				.httpBasic()
+				.and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.csrf().disable();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
