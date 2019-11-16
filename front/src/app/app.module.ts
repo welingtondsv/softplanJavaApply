ba1c72d314a52f249a5cb2326f159ded30e02a90
welington.vilhalva <welingtondsv78@gmail.com>
2019-11-16 14:26:32 -0400

    criado componentes para crud de pessoa

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListaComponent } from './pessoa/lista/lista.component';
+import { CadastroComponent } from './pessoa/cadastro/cadastro.component';
+import { EdicaoComponent } from './pessoa/edicao/edicao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    CadastroComponent,
+    EdicaoComponent
   ],
   imports: [
     BrowserModule
